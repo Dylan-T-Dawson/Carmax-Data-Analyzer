82ff2847c34a9d2e3e772abfe003d93a57053c8a
Dylan Dawson <120227014+Dylan-T-Dawson@users.noreply.github.com>
2023-01-06 11:58:36 -0500

    Formatting, renaming files, and adding README description

--- CarmaxAnalyzer.c
+++ CarmaxAnalyzer.c
@@ -1,16 +1,3 @@
-//The purpose of this project is to create an algorithm that can accurately predict certain aspects of the car
-//a customer will purchase based on the aspects of the car that they have traded in.
-//The first step in this process is to determine the consistency of each variable. ("How often do customers purchase a car of the same model?")
-//This will give us an idea of the areas on which to focus (No need to try to predict a variable that is not consistent in any way.
-//We will compare each variable in the traded in car to each variable in the purchased car to see if there is any relation. (Price to model, price to cylinder, price to etc... )
-//We will then use the best found relations for each variable in the purchased car to attempt to predict the purchased car's attributes.
-//Next, we will attempt to use varying numbers of the most correlated variables to predict the purchased car's attribtutes ("Do price and model of the trade in correlate to the price of the bought more than just price?")
-//Finally, we will create an AI that varies the weight placed on each of the variables and uses the previous information as a starting point. (If there is no correlation between model of trade in and cylinders of purchased, then the weight of model in predicting cylinders would be zero).
-//The AI will vary these weights in as many ways as feasible on my laptop in a reasonable amount of time until it determines the best possible combination of weights for each variable in prediciting each variables to produce the highest percentage of correct results based on the given dataset.
-//These weights will then be hardcoded into an algorithm capable of creating the most likely purchased car based on the traded in car.
-//This algorithm will be the deliverable of the project. From here we can advertise to customers based primarily on their traded in car and give them the best possible suggestion. This will help in securing purchases after a trade in. 
-//One problem may be ensuring that the algorithm does not create impossible cars ("Lambo with 4 cylinders and a $2 price). This should not happen actually because customers won't be buying impossible cars. Even if this were to happen, real cars with the most matching aspects could be promoted.
-//gcc CarmaxParser.c -o CarmaxParser
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,7 +6,7 @@
 #define ARRAYSIZE 1250 //Size of each array
 #define DATASIZE 200000 //Size of the data.
 
-//The following constants are to reference specific variables from the lines read in from the file.
+//The following constants are to reference specific variables from the lines read in from the file. They are passed as arguments to the "getIth" function.
 #define price 0
 #define appraisal_offer 1
 #define online_appraisal_flag 2
@@ -81,7 +68,7 @@ int main() {
     fp = fopen("ShowcaseDataWinter2023.csv", "r"); //ADD ERROR CHECKING
 
     //Gets and stores the title, which exceeds the normal MAXCHAR set length.
-    //The title is not needed, but the file pointer must be moved passe it.
+    //The title is not needed, but the file pointer must be moved passed it.
     char title[MAXCHAR*3];
     fgets(title, MAXCHAR*3, fp);
 
@@ -99,25 +86,10 @@ int main() {
         *masterArray[w] = toSetDef; //Sets an element of the masterArray's dereference (the memory previously allocated) equal to the value of toSetDef.
     }
 
-
-
-    //Now, the goal is to use each variable to predict the value of all the other variables. To do this for non-numeric or range variables, we will hold the value of the 
-    //variable in question constant at each possible value and determine the number of vehicles with this value of the variable in question with each possible variation of the
-    //response variable. For example, we will hold the appraise price constant at each interval respectively while iterating through the entire list. We will determine the number of vehicles
-    //matching the current appraised price whose model is x, y, z, etc... and whose cylinders are x, y, z, and whose ... Then, we will generate from this a model which will allow us to use the price
-    //to predict each of the other variables. For example, if there are 10 cars with a price appraisal between 5 and 10k, and of those 10, 3 lead to a x body, 6 and y body, and 1 a z body, the price 
-    //appraisal will generate a prediction that it is 30% x, 60% y, and 10% z. Then say that from the body we get an estimate that is 20% x, 60% z, and 20% y, we find the average of these values and 
-    //which ever variable has the highest percentage is our prediction. Once we have this model generated for all variables, we can then vary the weight on each variable and determine which variables
-    //are the best predictors of what. For example, maybe the trade-in trim is useless at predicting the number of cylinders of the purchased car, so it gets a weight of zero when calculating that variables,
-    //but perhaps it is an excellent predictor of the purchased car's trim, then it might have a weight of .25. For now, we will ignore the market and the online appraisal field.
-
-    //We will first test using just the appraisal price to predict the purchase price.
-    //Step 1 is to stratify all of the data into arrays.
-
-    /*Appraisal is from 0 to 40k+ and price is from 0 to 70k plus so the array is 9 * 15 = 135 in dimensions. Some values will not be used. 
+    /*Appraisal is from 0 to 40k+ and price is from 0 to 70k+ so the array is 9 * 15 = 135 in dimensions. Some values will not be used. 
     This is because the price variable has a range from $0k to $15k that is not broken down into $5k increments as is the rest of the data.
     A more complex method could possibly be used to save space, but unless this improvement is needed, the current implementation is sufficient. */
-    const int APROW = 9; //AP stands for Appraisal VS Price
+    const int APROW = 9; //The AP prefix represent Appraisal VS Price.
     const int APCOLUMN = 15;
     double priceArr[9 * 15]={0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -129,20 +101,20 @@ int main() {
                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     //In the above, the first row holds the price data for an appraisal of 0-5 etc...
-    //The columns the appraisal data for prices of 0-5, 5-10, etc...
+    //The columns hold the appraisal data for prices of 0-5, 5-10, etc...
     
     //Vaues to be used in the following loop.
     int val1;
     int val2;
-    //The following loop populates the price array based on the value of a the appraisal price and price from each line in the data set, which are stored in the master array.
+    //The following loop populates the price array based on the value of the appraisal price and price from each line in the data set, which are stored in the master array.
     for(int i = 0; i < numArrays; i++){
         for(int j = 0; j < ARRAYSIZE; j++){
             char buf1[MAXCHAR];
             char buf2[MAXCHAR];
-            getIth(masterArray[i]->arr[j], buf1, MAXCHAR, appraisal_offer);
-            getIth(masterArray[i]->arr[j], buf2, MAXCHAR, price);
-            val1 = getPrice(buf1);
-            val2 = getPrice(buf2);
+            getIth(masterArray[i]->arr[j], buf1, MAXCHAR, appraisal_offer); //Gets the appraisal from the current line
+            getIth(masterArray[i]->arr[j], buf2, MAXCHAR, price); //Gets the price from the current line
+            val1 = getPrice(buf1); //Converts appraisal string to int
+            val2 = getPrice(buf2); //Converts price string to int.
             //The following throws an error if the value of either the current appraisal offer or price is not within the range specified in the data.
             if(val1 > 70 || val1 < 0 || val2 > 70 || val2 < 0){
                 printf("\nError\n The line is %s", masterArray[i]->arr[j]);
@@ -155,8 +127,9 @@ int main() {
             priceArr[(val2/5) + (val1/5) * APCOLUMN]++;
 
         }
-        
-    } 
+    }
+    //Upon completion of the above loop, the priceArr is populated and each element reflects the total number of lines matching the respective criteria.
+    //For example, index 3 holds the total number of lines with an appraisal of $0k to $5k and a price of $15k to $20k.
     
     //We will now transform the array to hold percentages. Each row should total to 1.
     double rowTot;
@@ -169,14 +142,20 @@ int main() {
             priceArr[i*APCOLUMN + j] = priceArr[i*APCOLUMN+j] / rowTot;
         }
     }
+    //Now, each element in the priceArr holds the percentage of a respective price for a given appraisal.
+    //For example, if there were 100 appraisals of $15k to $20k and 10 of those had a price of $40k to $45k, then the element in the 4th row, 9th column would be 0.10.
 
-    //Now lets write a practice predictor
+    
+    //The following will NOT be used in the final version of this program, however, it is used to see how accurate the price can be predicted
+    //using the appraisal price.
     int valO;
     int valT;
     int rowNum;
     int maxI;
     int priceGuess;
     int numCorrect = 0;
+    //Loops through each element of the master array and compares the most likely price based on the appraisal price using the priceArr to
+    //the actual purchase price of each car.
      for(int i = 0; i < numArrays; i++){
         for(int j = 0; j < ARRAYSIZE; j++){
             char buf1[MAXCHAR];
@@ -187,29 +166,25 @@ int main() {
             valT = getPrice(buf2);
             rowNum = valO/5;
             maxI = maxInRow(priceArr, APROW, APCOLUMN, rowNum);
-            priceGuess = ((maxI % APCOLUMN)) * 5; //wrong
-            //printf("(%d, %d) ", priceGuess, maxI);
+            priceGuess = ((maxI % APCOLUMN)) * 5;
             if(priceGuess == valT){
                 numCorrect++;
             }
-            //printf("NumCorrect: %d, PriceGuess: %d, Val2: %d,\n", numCorrect, priceGuess, valT);
         }
      }
 
-     printf("Predicted price correctly %f%% of the time.", (double)numCorrect/DATASIZE * 100);
-     //We have achieved 25% accuracy at predicting the price of the vehicle.
+    printf("Predicted price correctly %f%% of the time.", (double)numCorrect/DATASIZE * 100);
+    //The algorithm has achieved 25% accuracy at predicting the price of the vehicle.
    
-     //The next step is to continue this for all parts of the trade in, using each one to predict the price of the purchased vehicle.
-     //We can act as if market is part of the appraisal and use it to predict the value of the trade in. The same can be done with whether or not the appraisal was done online.
-     //In addition, we can set our algorithm to online run on in person or online appraisal and see which is more accurate.
+    //The next step is to continue this for all aspects of the trade in, using each one to predict the price of the purchased vehicle.
 
-    //Next step is to clean this up and write a readme and publish to GitHub.
     fclose(fp);
     return 0;
 }
 
 void getIth(char* list, char* retVal, int max, int index){
     int cur = 0;
+    //Passes through index number of commas in the line.
     while(index != 0){
         if(list[cur] == ','){
             index--;
@@ -217,6 +192,7 @@ void getIth(char* list, char* retVal, int max, int index){
         cur++;
     }
    
+   //Stores all characters from the current comma to either the next comma or the end of the string into the return string.
     int i = 0;
     while(list[cur] != ',' && list[cur] != '\0' && cur < max){
         retVal[i] = list[cur];
@@ -229,19 +205,22 @@ void getIth(char* list, char* retVal, int max, int index){
 
 int getPrice(char* range){
     char priceStr[3];
+    //Setting index to 1 skips the dollar sign character.
     int index = 1;
+    //The loop and subsequent line store the numbers between the $ and first 'k'in priceStr
     while(index < 2 && range[index] != 'k'){
         priceStr[index-1] = range[index];
         index++;
     }
     priceStr[index-1] = range[index];
-    index = atoi(priceStr);
+    index = atoi(priceStr); //Converts priceStr to an integer value.
     return index;
 }
 
 int maxInRow(double arr[], int rows, int columns, int rowNum){
     double max = 0;
     int index;
+    //Iterates through each element in a given row from the passed in array and stores the index of the maximum element.
     for(int i = 0; i < columns; i++){
         if(arr[rowNum * columns + i] > max){
             max = arr[rowNum * columns + i];
